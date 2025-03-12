36c114348860685e6ef0828fd4da938508230c65
azartechnet <azartechnet@gmail.com>
2025-03-12 22:46:58 +0530

    updated

---
+++ Project1/src/Sample1.java
@@ -0,0 +1,13 @@
+
+public class Sample1 {
+
+	
+	public static void main(String[] args) {
+		int productid=1001;
+		String productname="apple";
+		
+		System.out.println("Your ProductId is::"+productid+"Your productName is::"+productname);
+
+	}
+
+}
